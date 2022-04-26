9573ca9aa9d76e8f841a8385ea54daf51cbb7d12
c_Door in <egor1@mail.ru>
2022-04-26 17:22:50 +0300

    Insert models and admin settings from past project

--- website/admin.py
+++ website/admin.py
@@ -1,3 +1,139 @@
 from django.contrib import admin
+from .models import Dish, Product, Subscribe, User, Preference, Allergy, Bill
+from django.db import models
+from django.db.models.functions import Trunc
+from django.db.models import Sum, Min, Max, DateTimeField
 
-# Register your models here.
+admin.site.register(Preference)
+admin.site.register(Allergy)
+
+
+@admin.register(Product)
+class Product(admin.ModelAdmin):
+    search_fields = ('title',)
+
+
+def get_next_in_date_hierarchy(request, date_hierarchy):
+    if date_hierarchy + '__day' in request.GET:
+        return 'hour'
+    if date_hierarchy + '__month' in request.GET:
+        return 'day'
+    if date_hierarchy + '__year' in request.GET:
+        return 'week'
+    return 'month'
+
+
+@admin.register(Bill)
+class BillAdmin(admin.ModelAdmin):
+    raw_id_fields = ('user', 'subscription')
+    list_display = ['user', 'price', 'creation_date']
+    list_filter = ['creation_date', 'subscription__sub_type']
+    change_list_template = 'admin/bill_admin_change_list.html'
+    date_hierarchy = 'creation_date'
+
+    def changelist_view(self, request, extra_context=None):
+        response = super().changelist_view(
+            request,
+            extra_context=extra_context,
+        )
+        try:
+            queryset = response.context_data['cl'].queryset
+        except (AttributeError, KeyError):
+            return response
+        metrics = {
+            'total': models.Count('subscription__sub_type'),
+            'total_sales': models.Sum('price'),
+        }
+        response.context_data['summary'] = list(
+            queryset
+                .values('subscription__sub_type')
+                .annotate(**metrics)
+                .order_by('-total_sales')
+        )
+
+        response.context_data['summary_total'] = dict(
+            queryset.aggregate(**metrics)
+        )
+
+        period = get_next_in_date_hierarchy(request, self.date_hierarchy)
+        response.context_data['period'] = period
+
+        summary_over_time = queryset.annotate(
+                period=Trunc(
+                    'creation_date',
+                    'day',
+                    output_field=DateTimeField(),
+                ),
+            ).values('period').annotate(total=Sum('price')).order_by('period')
+
+        summary_range = summary_over_time.aggregate(
+            low=Min('total'),
+            high=Max('total')
+        )
+
+        high = summary_range.get('high', 0)
+        low = summary_range.get('low', 0)
+        response.context_data['summary_over_time'] = [{
+            'period': x['period'],
+            'total': x['total'] or 0,
+            'percent': ((x['total'] or 0) - low) / (high - low) * 100 if high > low else 0,
+        } for x in summary_over_time]
+        print(response.context_data['summary_over_time'])
+
+        return response
+
+    class Meta:
+        model = Bill
+        fields = '__all__'
+
+
+class SubscriptionAdmin(admin.StackedInline):
+    model = Subscribe
+
+
+@admin.register(Dish)
+class DishAdmin(admin.ModelAdmin):
+    raw_id_fields = ('preferences', 'allergy')
+    list_filter = ('preferences', 'allergy')
+    search_fields = ('title',)
+
+
+@admin.register(User)
+class User(admin.ModelAdmin):
+    inlines = [
+        SubscriptionAdmin
+    ]
+    filter_horizontal = ('favorite_dishes', 'unloved_dishes')
+
+    class Meta:
+        model = User
+
+
+@admin.register(Subscribe)
+class Subscribe(admin.ModelAdmin):
+    list_filter = ('subscriber',)
+    raw_id_fields = ('subscriber', 'preference', 'allergy')
+    readonly_fields = ('allowed_dishes', 'shown_dishes')
+    filter_horizontal = ('allergy',)
+    fieldsets = (
+        ('Общее', {
+            'fields': (
+                'subscriber',
+                'preference',
+                'allergy',
+                'allowed_dishes',
+                'subscription_start',
+                ('sub_type', 'number_of_meals'),
+                'shown_dishes',
+            ),
+            'classes': ('extrapretty'),
+        }),
+    )
+
+    def allowed_dishes(self, obj):
+        return ', \n'.join([dish.title for dish in obj.select_available_dishes()])
+
+    allowed_dishes.short_description = 'Блюда, соответствующие условиям подписки'
+
+    class Meta:
+        model = Subscribe
--- website/models.py
+++ website/models.py
@@ -1,3 +1,207 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+
+class Preference(models.Model):
+    title = models.CharField('Предпочтения', max_length=200)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Предпочтение'
+        verbose_name_plural = 'Предпочтения'
+
+
+class Allergy(models.Model):
+    title = models.CharField('Аллергия', max_length=200)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Аллергия'
+        verbose_name_plural = 'Аллергии'
+
+
+class Product(models.Model):
+    title = models.CharField('Название продукта', max_length=200)
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Ингридиент'
+        verbose_name_plural = 'Ингридиенты'
+
+
+class Dish(models.Model):
+    title = models.CharField('Название блюда', max_length=200)
+    description = models.TextField('Описание Блюда')
+    cooking_method = models.TextField('Способ приготовления')
+    image = models.ImageField(
+        "Изображение",
+        null=True,
+        blank=True,
+    )
+    ingredients = models.ManyToManyField(
+        Product,
+        verbose_name='Ингридиенты',
+        related_name='dishes',
+    )
+    preferences = models.ForeignKey(
+        Preference,
+        verbose_name='Подходит для:',
+        related_name='appropriate_dishes',
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    allergy = models.ManyToManyField(
+        Allergy,
+        verbose_name='Не подходит для:',
+        related_name='inappropriate_dishes',
+    )
+    calories = models.PositiveSmallIntegerField(
+        'Калорийность',
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return f'{self.title}'
+
+    class Meta:
+        verbose_name = 'Блюдо'
+        verbose_name_plural = 'Блюда'
+
+
+class User(models.Model):
+    first_name = models.CharField('Имя', max_length=250)
+    last_name = models.CharField('Фамилия', max_length=250, blank=True)
+    chat_id = models.IntegerField('Chat id', unique=True)
+    phone = models.CharField('Номер телефона', max_length=20, blank=True)
+    favorite_dishes = models.ManyToManyField(Dish,
+                                             verbose_name='Любимые блюда',
+                                             related_name='favourite',
+                                             blank=True,
+                                             default=None)
+
+    unloved_dishes = models.ManyToManyField(Dish,
+                                            verbose_name='Нелюбимые блюда',
+                                            related_name='unloved',
+                                            blank=True,
+                                            default=None)
+
+    def __str__(self):
+        return f'ID: {self.chat_id} имя: {self.first_name} телефон: {self.phone}'
+
+    class Meta:
+        verbose_name = 'Подписчик'
+        verbose_name_plural = 'Подписчики'
+
+
+class Subscribe(models.Model):
+    title = models.CharField('Название', max_length=200)
+
+    SubscribeLengthChoices = (
+        ('1', '1'),
+        ('3', '3'),
+        ('6', '6'),
+        ('9', '9'),
+        ('12', '12')
+    )
+
+    subscriber = models.ForeignKey(
+        User,
+        verbose_name='Пользователь',
+        related_name='subscribes',
+        on_delete=models.CASCADE,
+    )
+    preference = models.ForeignKey(
+        Preference,
+        verbose_name='Предпочтения',
+        related_name='subscribes',
+        on_delete=models.CASCADE,
+    )
+    allergy = models.ManyToManyField(
+        Allergy,
+        verbose_name='Аллергии',
+        related_name='subscribes',
+        blank=True
+    )
+    number_of_meals = models.PositiveSmallIntegerField(
+        'Количество приемов пищи в день',
+    )
+
+    persons_quantity = models.PositiveSmallIntegerField(
+        'Количество человек, на которых должна быть рассчитана порция',
+        default=1
+    )
+
+    shown_dishes = models.ManyToManyField(
+        Dish,
+        verbose_name='Показанные блюда',
+        related_name='used_subscribes',
+        blank=True
+    )
+    sub_type = models.CharField(
+        'Тип подписки',
+        choices=SubscribeLengthChoices,
+        db_index=True,
+        default=12,  # TODO в случае ошибки скрипта, пользователь автоматически получает максимальную подписку!!!
+        max_length=25,
+    )
+    subscription_start = models.DateField(
+        # TODO do not allow blank!
+        'Подписка была приобретена',
+        default=now,
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return f'Подписка {self.pk} - {self.title} - {self.subscriber.first_name}'
+
+    def select_available_dishes(self):
+        allergens = [allergy for allergy in self.allergy.all()]
+        preference = self.preference
+        dishes = Dish.objects.filter(preferences=preference) \
+                             .exclude(allergy__in=allergens)
+        self.dishes = dishes
+
+        return self.dishes
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+
+
+class Bill(models.Model):
+
+    price = models.IntegerField(
+        'Стоимость заказа',
+        db_index=True,
+    )
+    user = models.ForeignKey(
+        User,
+        verbose_name='Чек пользователя',
+        related_name='user',
+        on_delete=models.CASCADE
+    )
+    subscription = models.ForeignKey(
+        Subscribe,
+        verbose_name='Относится к подписке',
+        related_name='subscription',
+        on_delete=models.CASCADE
+    )
+    creation_date = models.DateTimeField(
+        'Дата создания заказа',
+        db_index=True
+    )
+
+    def __str__(self):
+        return f'Чек пользователя {self.user.first_name}'
+
+    class Meta:
+        verbose_name = 'Продажа'
+        verbose_name_plural = 'Продажи'
