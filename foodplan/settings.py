da5715e5686e36c86d7e406d10cab4d028078183
rs0x069 <rs0x069@gmail.com>
2022-04-30 00:57:21 +0300

    Make base templates

--- foodplan/settings.py
+++ foodplan/settings.py
@@ -61,6 +61,7 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
             os.path.join(BASE_DIR, 'accounts/templates'),
+            os.path.join(BASE_DIR, 'payment/templates'),
             os.path.join(BASE_DIR, 'templates'),
         ],
         'APP_DIRS': True,
