c07e29d7ad2d41589cc97bd636853425b68903a6
c_Door in <egor1@mail.ru>
2022-04-30 23:57:49 +0300

    Get order details

--- payment/views.py
+++ payment/views.py
@@ -1,4 +1,5 @@
 import stripe
+from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
 from django.views.generic import TemplateView
@@ -43,6 +44,33 @@ def pay_success(request):
     #     )
 
     return render(request, "success.html")
+
+
+def get_allergies(order_details):
+    allergies = []
+    for order_param, param_value in order_details.items():
+        if 'allergy' in order_param:
+            allergies.append(param_value)
+    return allergies
+
+
+def order(request):   
+    order_details = request.POST
+
+    if order_details:
+        subscribe = {
+            'subscriber': request.user.id,
+            'allergy': get_allergies(order_details),
+            'number_of_meals': sum([int(order_details['first_meal']),
+                                   int(order_details['second_meal']),
+                                   int(order_details['third_meal']),
+                                   int(order_details['fourth_meal'])]),
+            'persons_quantity': order_details['persons_quantity'],
+            'sub_type': order_details['sub_type']
+        }
+        return HttpResponseRedirect(reverse('make_payment'))
+
+    return render(request, 'order.html')
 
 
 class CancelledView(TemplateView):
